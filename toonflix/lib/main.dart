40addee7669b3544e8625a2598efed5e45f86dba
JangHoon Park <gretea4489@gmail.com>
2023-07-12 18:31:01 +0900

    #3.2 Buttons Section

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -15,6 +15,7 @@ class App extends StatelessWidget {
             horizontal: 40,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -28,7 +29,7 @@ class App extends StatelessWidget {
                       Text(
                         'Hey, Selena',
                         style: TextStyle(
-                          color: Colors.white,
+                          color: Colors.amber,
                           fontSize: 28,
                           fontWeight: FontWeight.w800,
                         ),
@@ -42,6 +43,51 @@ class App extends StatelessWidget {
                       ),
                     ],
                   )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Blance',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 22,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '\$5 194 142',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      )),
                 ],
               ),
             ],
