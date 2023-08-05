c34cdc52887cd537258345083d4efc5f255a1de8
JangHoon Park <gretea4489@gmail.com>
2023-08-05 23:09:13 +0900

    쇼핑카트 디테일 뼈대 만들기

--- flutter_shoppingcart/lib/components/shoppingcart_detail.dart
+++ flutter_shoppingcart/lib/components/shoppingcart_detail.dart
@@ -5,6 +5,39 @@ class ShoppingCartDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Column(
+        children: [
+          _buildDetailNameAndPrice(),
+          _buildRatingAndReviewCount(),
+          _buildDetailColorOptions(),
+          _buildDetailButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailNameAndPrice() {
+    return const SizedBox();
+  }
+
+  Widget _buildRatingAndReviewCount() {
+    return const SizedBox();
+  }
+
+  Widget _buildDetailColorOptions() {
+    return const SizedBox();
+  }
+
+  Widget _buildDetailIcons() {
+    return const SizedBox();
+  }
+
+  Widget _buildDetailButton() {
     return const SizedBox();
   }
 }
