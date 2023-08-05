dfc0425182560e1980438ac9617548f9db0f5d1c
JangHoon Park <gretea4489@gmail.com>
2023-08-05 23:41:16 +0900

    쇼핑카트 디테일 만들기 첫번째

--- flutter_shoppingcart/lib/components/shoppingcart_detail.dart
+++ flutter_shoppingcart/lib/components/shoppingcart_detail.dart
@@ -10,23 +10,65 @@ class ShoppingCartDetail extends StatelessWidget {
         color: Colors.white,
         borderRadius: BorderRadius.circular(40),
       ),
-      child: Column(
-        children: [
-          _buildDetailNameAndPrice(),
-          _buildRatingAndReviewCount(),
-          _buildDetailColorOptions(),
-          _buildDetailButton(),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            _buildDetailNameAndPrice(),
+            _buildRatingAndReviewCount(),
+            _buildDetailColorOptions(),
+            _buildDetailButton(),
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildDetailNameAndPrice() {
-    return const SizedBox();
+    return const Padding(
+      padding: EdgeInsets.only(
+        bottom: 10.0,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Urban Soft AL 10.0",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "\$699",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Widget _buildRatingAndReviewCount() {
-    return const SizedBox();
+    return const Row(
+      children: [
+        Icon(Icons.star, color: Colors.yellow),
+        Icon(Icons.star, color: Colors.yellow),
+        Icon(Icons.star, color: Colors.yellow),
+        Icon(Icons.star, color: Colors.yellow),
+        Icon(Icons.star, color: Colors.yellow),
+        Spacer(),
+        Text("review "),
+        Text(
+          "(26)",
+          style: TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+      ],
+    );
   }
 
   Widget _buildDetailColorOptions() {
