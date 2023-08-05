6608842e2a3215ee8558fbf0c6c2d4d9e0d37f31
JangHoon Park <gretea4489@gmail.com>
2023-08-05 20:26:08 +0900

    쇼핑카드앱 - 앱 구조 살펴보기

---
+++ flutter_shoppingcart/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ShoppingCartPage(),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Hello, World!"),
+    );
+  }
+}
