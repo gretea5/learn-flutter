1aa8a56c195aef61d60af8b393417f402da94b87
JangHoon Park <gretea4489@gmail.com>
2023-08-05 17:43:47 +0900

    자잘한 프로젝트 삭제

--- responsive_lec/android/app/src/main/kotlin/com/example/responsive_lec/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.responsive_lec
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- responsive_lec2/android/app/src/main/kotlin/com/example/responsive_lec2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.responsive_lec2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
