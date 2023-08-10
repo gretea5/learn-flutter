5d8f2e71e763a46558ccacb382ed0e312b18d282
JangHoon Park <gretea4489@gmail.com>
2023-08-10 20:33:37 +0900

    #6.15 ~ #6.17 끝
    마무리

--- webtoon_app/lib/screens/detail_screen.dart
+++ webtoon_app/lib/screens/detail_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webtoon_app/models/webtoon_detail_model.dart';
 import 'package:webtoon_app/models/webtoon_episode_model.dart';
 import 'package:webtoon_app/services/api_service.dart';
+import 'package:webtoon_app/widgets/episode_widget.dart';
 
 class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
@@ -20,13 +22,48 @@ class _DetailScreenState extends State<DetailScreen> {
   
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences prefs;
+  bool isLiked = false;
 
   @override
   void initState() {
     super.initState();
     webtoon = APiService.getToonById(widget.id);
     episodes = APiService.getLatestEpisodesById(widget.id);
+    initPrefs();
   }
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      setState(() {
+        isLiked = likedToons.contains(widget.id);
+      });
+    }
+    else {
+      await prefs.setStringList('likedToons', []);
+    }
+  }
+
+
+  onHeartTap() async {
+    final likedToons = await prefs.getStringList('likedToons');
+    if (likedToons == null) {
+      await prefs.setStringList('likedToons', []);
+    }
+    if (isLiked) {
+      likedToons!.remove(widget.id);
+    }
+    else {
+      likedToons!.add(widget.id);
+    }
+    await prefs.setStringList('likedToons', likedToons);
+    setState(() {
+      isLiked = !isLiked;
+    });
+  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -44,74 +81,97 @@ class _DetailScreenState extends State<DetailScreen> {
         ),
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: onHeartTap, 
+            icon:isLiked ? Icon(Icons.favorite) : Icon(Icons.favorite_outline_outlined),
+          ),
+        ],
       ),
-      body: Column(children: [
-        const SizedBox(
-          height: 50,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Hero(
-              tag: widget.id,
-              child: Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      blurRadius: 15,
-                      offset:
-                          const Offset(10, 10), // changes position of shadow
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: widget.id,
+                  child: Container(
+                    width: 250,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          blurRadius: 15,
+                          offset:
+                              const Offset(10, 10), // changes position of shadow
+                        ),
+                      ],
                     ),
-                  ],
+                    child: Image.network(
+                      widget.thumb,
+                    ),
+                  ),
                 ),
-                child: Image.network(
-                  widget.thumb,
-                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if(snapshot.hasData) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "${snapshot.data!.genre} / ${snapshot.data!.age}",
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      
+                    ],
+                  );
+                }
+                return const Text("...");
+              },
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            FutureBuilder(
+              future: episodes,
+              builder: ((context, snapshot) {
+                if(snapshot.hasData) {
+                  return Column(
+                    children: [
+                      for(var episode in snapshot.data!) 
+                        Episode(episode: episode, id: widget.id,)
+                    ],
+                  );
+                }
+                return Container();
+              }
               ),
             ),
-          ],
+          ]),
         ),
-        const SizedBox(
-          height: 25,
-        ),
-        FutureBuilder(
-          future: webtoon,
-          builder: (context, snapshot) {
-            if(snapshot.hasData) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 50,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      snapshot.data!.about,
-                      style: const TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      "${snapshot.data!.genre} / ${snapshot.data!.age}",
-                      style: const TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            }
-            return const Text("...");
-          },
-        )
-      ]),
+      ),
     );
   }
 }
+
