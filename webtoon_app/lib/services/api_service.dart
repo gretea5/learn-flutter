b63be4411244deb708f25e490e9ae4da6cde84d4
JangHoon Park <gretea4489@gmail.com>
2023-08-10 18:53:02 +0900

    #6.12 ApiService

--- webtoon_app/lib/services/api_service.dart
+++ webtoon_app/lib/services/api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:webtoon_app/models/webtoon_detail_model.dart';
+import 'package:webtoon_app/models/webtoon_episode_model.dart';
 import 'package:webtoon_app/models/webtoon_model.dart';
 
 class APiService {
@@ -18,6 +20,32 @@ class APiService {
         webtoonInstances.add(toon);
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseURL/$id');
+    final response = await http.get(url);
+    if(response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(String id) async {
+    final List<WebtoonEpisodeModel> episodeInstances = [];
+    final url = Uri.parse('$baseURL/$id/episodes');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        final toon = WebtoonEpisodeModel.fromJson(episode);
+        episodeInstances.add(toon);
+      }
+      return episodeInstances;
     }
     throw Error();
   }
